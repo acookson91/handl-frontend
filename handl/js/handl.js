6e26b61e07b47232fea410b96b73d0e85353fa00
Simon Glancy <simonglancy@Simons-MacBook-Pro-2.local>
2016-05-03 18:26:33 +0100

    views

--- handl/js/handl.js
+++ handl/js/handl.js
@@ -1,1 +1,10 @@
-var handlApp = angular.module("handlApp", []);
+var handlApp = angular.module("handlApp", ["ngRoute","ngResource"]);
+
+handlApp.config(function($routeProvider,$locationProvider){
+  $locationProvider.html5Mode(true);
+  $routeProvider
+    .when("/", {
+      templateUrl: "/templates/home.html",
+      controller: 'mainHandlr'
+    })
+})
--- test/e2e/handlFeatures.js
+++ test/e2e/handlFeatures.js
@@ -3,4 +3,10 @@ describe('handl',function(){
     browser.get('/');
     expect(browser.getTitle()).toEqual('Handl App');
   });
+
+  it('new link leads to new delivery form', function(){
+    browser.get('/');
+    $("#new-delivery").click();
+    expect($("#delivery-form").isPresent()).toBeTruthy();
+  });
 });
