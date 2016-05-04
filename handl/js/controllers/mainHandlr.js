692063f7c8d3ed42a66150013d1bf23e68272db7
Andrew Cookson <acookson91@gmail.com>
2016-05-04 09:50:48 +0100

    Update mainHandlr.js

--- handl/js/controllers/mainHandlr.js
+++ handl/js/controllers/mainHandlr.js
@@ -1,16 +1,8 @@
 handlApp.controller('mainHandlr', ["$scope",function($scope){
-  var self = this;
-
-  self.deliveries2 = {};
-
   $scope.deliveries = {};
 
   $scope.addDeliveryDetails = function(delivery){
-    console.log(delivery);
     $scope.deliveries = angular.copy(delivery);
-    console.log($scope.deliveries.rPCode);
-
-    self.deliveries2 = delivery;
   };
 
 
