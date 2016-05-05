0c247bd49d7ad14aa914fc38c51d457e3c040c2b
Sachin Karia <sachin.karia01@gmail.com>
2016-05-05 12:36:52 +0100

    flash successful and failed delivery create request

--- handl/js/controllers/mainHandlr.js
+++ handl/js/controllers/mainHandlr.js
@@ -1,6 +1,9 @@
 handlApp.controller('mainHandlr', [ "$scope", "$location", "deliveryFactory", "addDeliveryService", function($scope, $location, deliveryFactory, addDeliveryService){
   var self = this;
   $scope.deliveries = [];
+  $scope.alertMessage = false;
+  $scope.alertMessageFail = false;
+
 
 
   $scope.addDeliveryDetails = function(delivery){
@@ -9,10 +12,13 @@ handlApp.controller('mainHandlr', [ "$scope", "$location", "deliveryFactory", "a
   };
 
   $scope.confirmDelivery = function(delivery){
-    addDeliveryService.create(delivery);
+    addDeliveryService.create(delivery)
+
+    .success(function(){
+      $scope.alertMessage = true;
+    })
+    .error(function(){
+      $scope.alertMessageFail = true;
+    });
   };
-
-
-
-
 }]);
