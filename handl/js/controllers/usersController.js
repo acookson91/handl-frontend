f8c5a259594d332a528a7d161ba522bbecffe636
Gabrielle <gabivk@gmail.com>
2016-05-12 10:00:06 +0100

    routing success!

--- handl/js/controllers/usersController.js
+++ handl/js/controllers/usersController.js
@@ -1,20 +1,16 @@
 handlApp.controller('usersController', ['ipCookie','$scope', '$state', '$auth',
-  function(ipCookie ,$scope, $state, $auth){
+  function(ipCookie, $scope, $state, $auth){
 
     $scope.handleRegBtnClick = function() {
       $scope.registrationForm.handlr_status = false;
       $auth.submitRegistration($scope.registrationForm)
         .then(function() {
-          console.log($scope.registrationForm);
-          console.log(status);
           $auth.submitLogin({
             name: $scope.registrationForm.name,
             email: $scope.registrationForm.email,
             password: $scope.registrationForm.password,
             handlr_status: $scope.registrationForm.handlr_status
           });
-
-          $state.go('user.deliveries');
         });
 
     };
